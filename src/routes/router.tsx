3679da61008852541e652ff0a6e5ee400b9183e4
sabilar-rahman <sabilarrahman36@gmail.com>
2024-09-23 01:48:15 +0600

    dashboard

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,17 @@
-// import path from "path";
+ import path from "path";
 import { createBrowserRouter } from "react-router-dom";
 import RootMain from "./RootMain";
+import Dashboard from "@/pages/Dashboard/Dashboard";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <RootMain />,  
+        element: <RootMain />, 
+        children: [
+            {path:'dashboard',
+                element:<Dashboard/>
+            }
+        ], 
 
     }
     
